4231ff681908117e1e7d04c0f171c4c6134e4ce4
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-08-31 17:36:20 +0200

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require( 'express' );
+const { response, json } = require( 'express' );
 const bcryptjs = require( 'bcryptjs' );
 const Usuario = require('../models/usuario' );
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async ( req, res = response ) => {
@@ -49,11 +50,58 @@ const login = async ( req, res = response ) => {
         });
         
     }
+}
 
+const googleSignIn = async( req, res = respones ) => {
+    
+    const { id_token } = req.body;    
+
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER_ROLE',
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if ( !usuario.estado ) {
+            return res.status( 401 ).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+        
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) { 
+        console.log( error );
+
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar'
+        });    
+    }
 
 }
 
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
