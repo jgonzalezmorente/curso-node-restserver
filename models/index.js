67aa516f7ae6c569eb56d573eb4977f3bd806ca0
José Antonio González Morente <jgonzalezmorente@gmail.com>
2023-04-21 14:45:41 +0200

    Error exportación modelo Producto

--- models/index.js
+++ models/index.js
@@ -1,9 +1,8 @@
 const Categoria = require( './categoria' );
-const Producto  = require( './Producto' );
+const Producto  = require( './producto' );
 const Role      = require( './role' );
 const Server    = require( './server' );
 const Usuario   = require( './usuario' );
-
 
 module.exports = {
     Categoria,
